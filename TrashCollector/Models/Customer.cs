b467fb7b318147bbc16e2216a1e2b84faba45731
DonaldZheng <dzheng112@gmail.com>
2021-03-19 15:19:34 -0500

    added my keys in my customer class

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -15,9 +15,54 @@ namespace TrashCollector.Models
         {
             get; set;
         }
-        public string Name
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is a Required Input")]
+        public string FirstName
         {
             get; set;
+        }
+
+        [Display(Name = "Last Name")]
+        public string LastName
+        {
+            get; set;
+        }
+
+        [Display(Name = "StreetName")]
+        public string StreetName
+        {
+            get; set;
+        }
+
+        [Display(Name = "City")]
+        public string City
+        {
+            get; set;
+        }
+        [Display(Name = "ZipCode")]
+        public string ZipCode
+        {
+            get; set;
+        }
+        
+        [Display(Name = "Weekly Pickup Date")]
+        public DayOfWeek? PickUpDate
+        {
+            get; set;
+        }
+       
+
+        public string PickUpTime
+        {
+            get; set;
+
+        }
+      
+        public string Payment //
+        {
+            get; set;
+
         }
         [ForeignKey("IdentityUser")]
         public string IdentityUserId
